c738ee47667236414f41b522ba979527e94c1aca
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-05-02 21:33:04 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,9 @@
-const inputEl = document.querySelector("#font-size-control");
-const textEl = document.querySelector("#text");
+const inputEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+inputEl.value = 16;
 console.log(inputEl.value);
 console.log(textEl.attributes);
 
-inputEl.addEventListener("input", () => {
-  textEl.style.fontSize = inputEl.value + "px";
+inputEl.addEventListener('input', () => {
+  textEl.style.fontSize = inputEl.value + 'px';
 });
