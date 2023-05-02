80ba41cec15912f81742c2a39c0e8d423b3fe6ba
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-05-02 21:33:02 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,13 @@
-const inputEl = document.querySelector("#validation-input");
+const inputEl = document.querySelector('#validation-input');
 
-console.log(inputEl.id);
-
-inputEl.addEventListener("blur", () => {
+inputEl.addEventListener('blur', () => {
   if (+inputEl.dataset.length === inputEl.value.length) {
-    inputEl.setAttribute("id", "#validation-input.valid");
+    inputEl.classList.add('valid');
 
-    inputEl.removeAttribute("#validation-input.invalid");
+    inputEl.classList.remove('invalid');
   } else {
-    inputEl.setAttribute("id", "#validation-input.invalid");
+    inputEl.classList.add('invalid');
 
-    inputEl.removeAttribute("#validation-input.valid");
+    inputEl.classList.remove('valid');
   }
 });
